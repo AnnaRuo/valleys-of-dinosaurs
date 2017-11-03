100877409be74add258e560e9f3c824a75fd1bed
Anna <anna.ruotanen@gmail.com>
2017-11-03 16:31:58 +0100

    Edit, update and delete dinosaurs

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root "dinosaurs#index"
+  resources :dinosaurs
 
-  get "dinosaurs" => "dinosaurs#index"
-  get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaur
-  get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
-
-  post "dinosaurs" => "dinosaurs#create"
+  # get "dinosaurs" => "dinosaurs#index"
+  # get 'dinosaurs/new' => "dinosaurs#new", as: :new_dinosaur
+  # get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
+  # post "dinosaurs" => "dinosaurs#create"
+  # get 'dinosaurs/:id/edit' => 'dinosaurs#edit', as: :edit_dinosaur
+  # patch "dinosaurs/:id" => "dinosaurs#update"
+  # delete "dinosaurs/:id" => "dinosaurs#destroy"
 end
